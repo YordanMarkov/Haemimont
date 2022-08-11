853f228e16a335e9a6b40d049a5c80ee7c40cba6
YordanMarkov <yordanmarkov2004@gmail.com>
2022-08-11 14:11:06 +0300

    ToDo ver 2.3 - working

--- ToDo/app.js
+++ ToDo/app.js
@@ -1,6 +1,55 @@
 const app = Vue.createApp({
     data() {
         return {
+            id: 0
+        }
+    },
+    methods: {
+        listen() {
+            this.id = 1;
+        }
+    }
+    ,
+    template: `
+    <div>
+        <div v-if="id === 0">
+            <login  @login="listen" />
+        </div>
+        <div v-if="id === 1">
+            <task></task>
+        </div>
+    </div>
+    `
+})
+
+app.component('login', {
+    data() {
+        return {
+            info: {
+                user: null,
+                password: null,
+                id: 0
+            }
+        }
+    },
+    template: `
+      <div>
+            <h1>ToDo: Task Manager</h1>
+            <b>Log in in order to edit/view your tasks.</b>
+            
+            <input v-model="info.user" placeholder="Username: e.g. 'ToDo'"/>
+            
+            <input v-model="info.password" placeholder="Password: e.g. 'T0d0P@ssw0rD'"/>
+            <div v-if="info.user !== null && info.password !== null">
+                <button @click="$emit('login')">Log in</button>
+            </div>        
+      </div>
+    `
+})
+
+app.component('task', {
+    data() {
+        return {
             list: [],
             task: {
                 title: null,
@@ -8,11 +57,6 @@ const app = Vue.createApp({
                 description: null,
                 done: 0
             },
-            info: {
-                user: null,
-                password: null,
-                id: 0
-            }
         }
     },
     methods: {
@@ -37,42 +81,24 @@ const app = Vue.createApp({
         }
     },
     template: `
-    <div v-if="info.id === 0">
-        <!-- <login  @login="info.id = 1" /> -->
+            <h1>ToDo: Task Manager</h1>
+            <b>Use the remove button to mark as done.</b>
 
-        <h1>ToDo: Task Manager</h1>
-        <b>Log in in order to edit/view your tasks.</b>
-        
-        <input v-model="info.user" placeholder="Username: e.g. 'ToDo'"/>
-        
-        <input v-model="info.password" placeholder="Password: e.g. 'T0d0P@ssw0rD'"/>
-        <div v-if="info.user !== null && info.password !== null">
-            <!-- <button @click="$emit('login')">Log in</button> -->
-            <button v-on:click="info.id = 1">Log in</button>
-        </div>
-    </div>
-
-    <div v-if="info.id === 1">
-        <h1>ToDo: Task Manager</h1>
-        <b>Use the remove button to mark as done.</b>
-
-        <p>Insert task:</p>
-        <input v-model="task.title" placeholder="Title: e.g. 'Task 1'"/>
-        <input v-model="task.dueDate" placeholder="Date: e.g. '2022-08-30'"/>
-        <input v-model="task.description" placeholder="Description: e.g. 'Code'"/>
-        <div v-if="task.title !== null && task.dueDate !== null && task.description !== null">
-            <button v-on:click="insert()">Push</button>
-        </div>
-        <ul id="list">
-            <li v-for="t in list">
-                Title: {{ t.title }} 
-                Due date: {{t.dueDate}}  
-                Description: {{t.description}} 
-                <button v-on:click="remove(t)">Remove</button>
-            </li>
-        </ul>
-    </div>
+            <p>Insert task:</p>
+            <input v-model="task.title" placeholder="Title: e.g. 'Task 1'"/>
+            <input v-model="task.dueDate" placeholder="Date: e.g. '2022-08-30'"/>
+            <input v-model="task.description" placeholder="Description: e.g. 'Code'"/>
+            <div v-if="task.title !== null && task.dueDate !== null && task.description !== null">
+                <button v-on:click="insert()">Push</button>
+            </div>
+            <ul id="list">
+                <li v-for="t in list">
+                    Title: {{ t.title }} 
+                    Due date: {{t.dueDate}}  
+                    Description: {{t.description}} 
+                    <button v-on:click="remove(t)">Remove</button>
+                </li>
+            </ul>
     `
 })
 app.mount('#app')
-
